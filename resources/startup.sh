f20e883bf30c634df2775b318708e2a1f42e003c
khs <khs@khs-15ND540-UX7MK>
2016-09-28 15:07:41 +0900

    -

--- resources/startup.sh
+++ resources/startup.sh
@@ -1,1 +1,1 @@
-docker run -it -h jeus5 --name jeus5 -p 9744:9744 -v $(pwd)/data/config/vhost.xml:/root/jeus5/config/vhost.xml khs/jeus:5_ubuntu14.04.5_jdk6
+docker run -it -h jeus5 --name jeus5 -p 9744:9744 -v $(pwd)/data/config/vhost.xml:/root/jeus5/config/vhost.xml khs/jeus
